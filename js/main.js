b646a9ba62b48ad9e731344f401a4d7d534f32fa
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-22 18:44:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-let button = document.querySelector('#button');
-let par = document.querySelector('p');
-let name1 = document.getElementById('name');
-let surname1 = document.getElementById('surname');
-let phone1 = document.getElementById('phone');
 
-button.onclick = function(){
-    console.log(button('p'));
-}
