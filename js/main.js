6a2d20dff6dcd5de0158cce28f6d3cef1d79f2f7
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-23 10:49:09 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+let button = document.getElementById('button');
+let dane = document.getElementById('dane');
+let name = document.getElementById('name').value;
+let surname = document.getElementById('surname').value;
+let phone = document.getElementById('phone').value;
+let container = document.getElementById('container');
 
+button.addEventListener (function(){
+
+ 
+  
+});  
+   
